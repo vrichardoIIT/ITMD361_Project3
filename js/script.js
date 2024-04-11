30dd6e9a20ae694807948737f55671ba15e2a0e4
Vincent Richardo <vrichardo@hawk.iit.edu>
2024-04-11 02:40:17 -0500

    js edit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 var map;
 
 function initMap() {
-  map = new Map(document.getElementById("canvas"), {
+  map = new google.maps.Map(document.getElementById('canvas'), {
     center: { lat: -34.397, lng: 150.644 },
     zoom: 8,
   });
