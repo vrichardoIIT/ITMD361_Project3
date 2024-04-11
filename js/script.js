f26134aeb5b06e59f20d77ff5f9f72b863c4acee
Vincent Richardo <vrichardo@hawk.iit.edu>
2024-04-11 02:38:11 -0500

    js edit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 var map;
 
 function initMap() {
-  map = new Map(document.getElementById("map"), {
+  map = new Map(document.getElementById("canvas"), {
     center: { lat: -34.397, lng: 150.644 },
     zoom: 8,
   });
