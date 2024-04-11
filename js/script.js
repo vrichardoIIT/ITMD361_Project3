b1cb23ad39dea3683e9742417e1dd8b60c993be7
Vincent Richardo <vrichardo@hawk.iit.edu>
2024-04-11 03:44:43 -0500

    map makrer on iit and different spots to visit in chicago

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
 var map;
+var iitmark;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById('canvas'), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
+    center: {lat: 41.83500496774838, lng: -87.62698444559126},
+    zoom: 12,
+  });
+
+  iitmark = new google.maps.Marker({
+    map: map,
+    position: {lat: 41.83500496774838, lng: -87.62698444559126}
   });
 }
